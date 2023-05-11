8ef829be490d0366d9db7815adfa6e455caf8584
Jan Boldis <jboldis.forwarding@gmail.com>
2023-05-11 21:44:28 +0200

    done

--- base/Resources/Public/js/setup.js
+++ base/Resources/Public/js/setup.js
@@ -1,5 +1,5 @@
 const body = document.body;
-const html = document.querySelector("html");
+const html = document.querySelector('html');
 var hash = location.hash.substr(1);
 var position = window.pageYOffset;
 
@@ -14,13 +14,13 @@ var getElemDistance = function (elem) {
   return location >= 0 ? location : 0;
 };
 
-if (hash !== "") {
+if (hash !== '') {
 } else {
-  html.style.scrollBehavior = "smooth";
+  html.style.scrollBehavior = 'smooth';
   window.__forceSmoothScrollPolyfill__ = false;
 }
-window.addEventListener("DOMContentLoaded", function () {
-  if (hash !== "") {
+window.addEventListener('DOMContentLoaded', function () {
+  if (hash !== '') {
     this.setTimeout(() => {
       var elem = document.getElementById(hash);
       var location = getElemDistance(elem);
@@ -28,37 +28,10 @@ window.addEventListener("DOMContentLoaded", function () {
       console.log(location);
     }, 1000);
   }
-  var headings = document.querySelectorAll("header *"),
+  var headings = document.querySelectorAll('header *'),
     _nv;
   for (var i = headings.length - 1; i >= 0; i--) {
     _nv = headings[i].innerHTML;
-    headings[i].innerHTML = _nv.replace(/\|/, "&shy;");
+    headings[i].innerHTML = _nv.replace(/\|/, '&shy;');
   }
 });
-const scrollIndicator = document.querySelector("body");
-const footer = document.querySelector("footer");
-if (footer) {
-  function getFooterHeight() {
-    return footer.offsetHeight + "px";
-  }
-
-  scrollIndicator.style.setProperty("--footer-calculated-height", getFooterHeight());
-  window.addEventListener("scroll", (event) => {
-    scrollIndicator.style.setProperty("--footer-calculated-height", getFooterHeight());
-  });
-}
-var menu = document.getElementById("menu");
-var toggleButtons = document.querySelectorAll(".nav-link-toggle");
-toggleButtons.forEach((link) => {
-  link.addEventListener("click", function (e) {
-    toggleButtons.forEach((currentLink) => {
-      if (currentLink !== link) currentLink.parentElement.classList.remove("open");
-    });
-    e.preventDefault();
-    link.parentElement.classList.toggle("open");
-  });
-});
-function toggleMenu(button) {
-  menu.classList.toggle("-translate-y-full");
-  button.setAttribute("aria-expanded", button.getAttribute("aria-expanded") === "true" ? "false" : "true");
-}
